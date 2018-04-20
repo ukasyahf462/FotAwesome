ac7f7a1f7eef53a9331332eb0a2a80bfa61e1668
ukasyahf462 <32339489+ukasyahf462@users.noreply.github.com>
2018-04-21 03:22:09 +0700

    Add files via upload

---
+++ Fontawesome.js
@@ -0,0 +1,7 @@
+﻿ (function() {
+   var css = document.createElement('link');
+   css.href = '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+   css.rel = 'stylesheet';
+   css.type = 'text/css';
+   document.getElementsByTagName('head')[0].appendChild(css);
+  })();
